ec406bc5d1187217bb732c9be28daa0de324fa53
Lars-Dominik Braun <ldb@leibniz-psychology.org>
2020-12-14 13:43:16 +0100

    yamunanagar: Add channel introduction to unattended upgrades

--- src/zpid/machines/yamunanagar/os.scm
+++ src/zpid/machines/yamunanagar/os.scm
@@ -75,7 +75,12 @@
                                (unattended-upgrade-configuration
                                 (channels #~(cons* (channel
                                                     (name 'psychnotebook-deploy)
-                                                    (url "https://github.com/leibniz-psychology/psychnotebook-deploy.git"))
+                                                    (url "https://github.com/leibniz-psychology/psychnotebook-deploy.git")
+                                                    (introduction
+                                                     (make-channel-introduction
+                                                      "02ae8f9f647ab9650bc9211e728841931f25792c"
+                                                      (openpgp-fingerprint
+                                                       "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
                                              %default-channels))
                                  (operating-system-file
                                   (scheme-file "config.scm"
