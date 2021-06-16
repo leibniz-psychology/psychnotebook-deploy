12244b33bb2c0d3a4972af4407f5ef60d5f9aaf2
Lars-Dominik Braun <ldb@leibniz-psychology.org>
2021-06-16 13:52:26 +0200

    Add auditing tool

---
+++ tools/audit/audit-mail.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+users=`journalctl -o cat -u usermgrd.service -S yesterday -U today | sed -rne 's#.* ((Creat|Delet)ed .*)#\1#gp'`
+projects=''
+
+projects=`find /storage/public/ -perm -o=r -type f -path '**/.config/workspace.yaml' -ctime '-1' | while read -r L; do
+	sed -nre 's#^_id: ([a-z-]+)#https://www.psychnotebook.org/workspaces/\1#gp' "$L" && stat -c '%U' "$L" && echo
+done`
+
+if [ -z "$users"] && [ -z "$projects" ]; then
+	# nothing to do
+	exit 0
+fi
+
+cat <<EOF | /usr/bin/msmtp -t
+Subject: [PsychNotebook] Audit logs `date -I`
+To: <psychnotebook@leibniz-psychology.org>
+From: <donot-reply@psychnotebook.org>
+Content-Type: text/plain; charset=utf-8
+Content-Disposition: inline
+Content-Transfer-Encoding: 8bit
+
+New/deleted users:
+
+$users
+
+New/changed public projects:
+
+$projects
+
+∎
+EOF
