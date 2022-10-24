f9d81f2dcc6479714deac6fdb1773a0eaa97ca11
Lars-Dominik Braun <ldb@leibniz-psychology.org>
2022-10-24 13:16:30 +0200

    yamunanagar: Add guix-cran to CI.

--- src/zpid/machines/yamunanagar/ci.scm
+++ src/zpid/machines/yamunanagar/ci.scm
@@ -17,85 +17,79 @@
 ;; Should be part of the service configuration
 (define channels
   (scheme-file "channels-for-build-jobs.scm"
-	       #~(append
-		   (list (channel
-			   (name 'guix-science)
-			   (url "https://github.com/guix-science/guix-science.git")
-			   (introduction
-			     (make-channel-introduction
-			       "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
-			       (openpgp-fingerprint
-				 "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
-			 (channel
-			   (name 'guix-zpid)
-			   (url "https://github.com/leibniz-psychology/guix-zpid.git")
-			   ))
-		   %default-channels)))
+           #~(append
+           (list
+             (channel
+               (name 'guix-science)
+               (url "https://github.com/guix-science/guix-science.git")
+               (introduction
+                 (make-channel-introduction
+                   "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
+                   (openpgp-fingerprint
+                 "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+             (channel
+               (name 'guix-zpid)
+               (url "https://github.com/leibniz-psychology/guix-zpid.git"))
+             (channel
+               (name 'guix-cran)
+               (url "https://github.com/guix-science/guix-cran.git")))
+           %default-channels)))
 
 (define manifest
   (scheme-file "manifest-for-build-jobs.scm"
-	       #~(begin
-		   (use-modules (guix packages) (srfi srfi-1))
-
-		   (define not-package?
-		     (lambda (p) (not (package? p))))
-
-		   (define module->packages
-		     (lambda (mod)
-		       (remove not-package?
-			       (hash-map->list (lambda (name var) (variable-ref var))
-					       (module-obarray (resolve-interface mod))))))
-
-		   (define packages
-		     (map module->packages
-			  (list '(guix-science packages jasp)
-				'(guix-science packages rstudio)
-				'(guix-science packages jupyter)
-				'(guix-science packages cran)
-				'(guix-science packages bioinformatics)
-				'(zpid packages cran)
-				'(zpid packages rstudio)
-				'(zpid packages psychnotebook)
-				'(zpid packages sanic)
-				'(zpid packages tortoise))))
-
-		   (packages->manifest (apply append packages)))))
+    #~(begin
+      (use-modules (guix discovery)
+                   (gnu packages)
+                   (guix modules)
+                   (guix utils)
+                   (guix packages))
+      (let* ((modules (all-modules (%package-module-path)))
+             (packages (fold-packages
+                         (lambda (p accum)
+                           (let ((mod (file-name->module-name (location-file (package-location p)))))
+                             (if (member (car mod) '(guix-cran guix-science zpid))
+                               (cons p accum)
+                               accum)))
+                         '() modules)))
+        (packages->manifest packages)))))
 
 (define channel-builder-shepherd-service
   (lambda (config)
     (match config
-	   (#f
-	    (let* ((code (with-imported-modules
-			   (source-module-closure '((guix build utils)))
-			   #~(begin
-			       ;; for invoke
-			       (use-modules (guix build utils))
-			       (define (loop)
-				 (system* #$(file-append guix "/bin/guix")
-					 "time-machine"  "-C" #$channels
-					 "--" "build" "--timeout=7200" "--keep-going" "-m" #$manifest)
+       (#f
+        (let* ((code (with-imported-modules
+               (source-module-closure '((guix build utils)))
+               #~(begin
+                   ;; for invoke
+                   (use-modules (guix build utils))
+                   (define (loop)
+                 (system* #$(file-append guix "/bin/guix")
+                     "time-machine"  "-C" #$channels
+                     "--" "build" "--fallback" "--timeout=7200" "--keep-going" "-M" "4"
+                          "--no-substitutes"
+                          "-m" #$manifest)
                    ;; Make sure substitutes are available by requesting them once from guix-publish.
                    (system* #$(file-append guix "/bin/guix")
-					 "time-machine"  "-C" #$channels
-					 "--"
+                     "time-machine"  "-C" #$channels
+                     "--"
                      "weather"
                      "--substitute-urls=https://substitutes.guix.psychnotebook.org"
                      "-m" #$manifest)
-				 (sleep 60)
-				 (loop))
-			       (loop))))
-		   (runner (program-file "channel-builder-runner" code)))
+                 (sleep 60)
+                 (loop))
+                   (loop))))
+           (runner (program-file "channel-builder-runner" code)))
 
-	      (list (shepherd-service
-		      (provision '(channel-builder))
-		      (documentation "Run the channel builder")
-		      (requirement '(user-processes networking))
-		      (start #~(make-forkexec-constructor
-				 (list #$runner)
-				 #:user "channelbuilder"
-				 #:group "channelbuilder"
-				 #:log-file "/var/log/channel-builder.log"))
-		      (stop #~(make-kill-destructor)))))))))
+          (list (shepherd-service
+              (provision '(channel-builder))
+              (documentation "Run the channel builder")
+              (requirement '(user-processes networking))
+              (start #~(make-forkexec-constructor
+                 (list #$runner)
+                 #:user "channelbuilder"
+                 #:group "channelbuilder"
+                 #:log-file "/var/log/channel-builder.log"))
+              (stop #~(make-kill-destructor)))))))))
 
 (define %channel-builder-accounts
   (list (user-group (name "channelbuilder") (system? #t))
